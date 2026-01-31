a674fc16bf6c92c67b59489006187bc7a192b33f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-31 20:51:05 +0800

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::{env, time::Duration};
+
 use anyhow::Result;
 use axum::{Router, response::Html, routing::get};
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 
 mod utils;
@@ -11,9 +14,23 @@ async fn handler() -> Html<&'static str> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+  dotenvy::dotenv().ok();
   tracing_subscriber::fmt().init();
 
-  let app = Router::new().route("/", get(handler));
+  // TODO: DatabaseConnection
+  // https://github.com/tokio-rs/axum/blob/main/examples/sqlx-postgres/src/main.rs
+  let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .acquire_timeout(Duration::from_secs(3))
+    .connect(
+      env::var("DATABASE_URL")
+        // TODO: unwrap_or_else
+        .expect("plast-mem: invalid database url")
+        .as_str(),
+    )
+    .await?;
+
+  let app = Router::new().route("/", get(handler)).with_state(pool);
   let listener = TcpListener::bind("0.0.0.0:3000").await?;
 
   tracing::info!("server started at http://0.0.0.0:3000");
